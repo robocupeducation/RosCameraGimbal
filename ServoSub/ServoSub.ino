c8da0051c1f20c7054b9814de187fa9e2186c9f1
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-05-28 13:24:48 +0200

    [ServoSub.ino]

---
+++ ServoSub/ServoSub.ino
@@ -0,0 +1,30 @@
+// Arduino waits for someone who had published in toggle_led topic and then blinks 
+// led attached at 13 pin.
+
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle  nh;
+
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  // digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+}
+
+ros::Subscriber<std_msgs::Empty> sub("/toggle_led", messageCb );
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  nh.spinOnce();
+}

7e98d225e8b874c311b632b3ed2b00c435b94707
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-05-28 16:59:03 +0200

    [ServoSub.ino] Servocontrol class has been created

--- ServoSub/ServoSub.ino
+++ ServoSub/ServoSub.ino
@@ -6,43 +6,55 @@
 #include <std_msgs/Empty.h>
 #include <Servo.h>
 
-ros::NodeHandle  nh;
-Servo XServo;  // crea el objeto servo
-int pos = 0;    // posicion del servo
 
-void messageCb( const std_msgs::String &msg){
-  // digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  digitalWrite(13, HIGH);
-  for (pos = 0; pos <= 180; pos++){
-    myservo.write(pos);              
-    delay(15);                       
-  }
-    for (pos = 180; pos > 0; pos--){
-    myservo.write(pos);              
-    delay(15);                       
-  }
-  digitalWrite(13, LOW);
+namespace CameraTools
+{
+class Servocontrol
+{
+  public:
+    Servocontrol(int servoX, int servoY): sub("/Servocontrol", &CameraTools::messageCallback,this){
+        pos = 0;
+        XServo.attach(servoX);
+        YServo.attach(servoY);
+        nh_.initNode();
+        nh_.subscribe(sub);
+    }
+
+    void listening(){
+      nh_.spinOnce();
+    }
+
+    void messageCallback( const std_msgs::String &msg){
+      digitalWrite(13, HIGH);
+      for (pos = 0; pos <= 360; pos++){
+        XServo.write(pos);
+        delay(15);
+      }
+        for (pos = 360; pos > 0; pos--){
+        XServo.write(pos);
+        delay(15);
+      }
+      digitalWrite(13, LOW);
+    }
+
+  private:
+    ros::NodeHandle nh_;
+    ros::Subscriber <std_msgs::String,CameraTools::Servocontrol> sub;
+    //ros::Subscriber <std_msgs::String> sub;
+    Servo XServo;
+    Servo YServo;
+    int pos;
+};
 }
-
-
-// void messageCb( const std_msgs::String &msg){
-//   // digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-//   digitalWrite(13, HIGH);
-//   delay(1000);
-//   digitalWrite(13, LOW);
-// }
-
-ros::Subscriber<std_msgs::String> sub("/Servocontrol", messageCb );
+  CameraTools::Servocontrol controller(9,10);
 
 void setup()
 {
+;
   pinMode(13, OUTPUT);
-  XServo.attach(9);
-  nh.initNode();
-  nh.subscribe(sub);
 }
 
 void loop()
 {
-  nh.spinOnce();
+  controller.listening();
 }
